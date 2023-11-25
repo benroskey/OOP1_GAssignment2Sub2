418bb60f84e9085f66b37d67791c67954508fed7
Benjamin Roskey <benjamin.roskey@edu.sait.ca>
2023-11-24 19:48:30 -0700

    Revert "MyStack Comments"
    This reverts commit 6ddec177a6c47b6d707529bc198dd91eb032c4ac.

--- CreatingADTs/src/utility/MyStack.java
+++ CreatingADTs/src/utility/MyStack.java
@@ -7,16 +7,6 @@ import adt.StackADT;
 public class MyStack<E> implements StackADT<E> {
     private MyArrayList<E> list;
 
-    /**
-     * Constructs an empty list with an initial capacity of ten.
-     * 
-     * @param none
-     * @return none
-     * @throws none
-     * @postcondition none
-     * @precondition none
-     * @complexity O(1)
-     */
     public MyStack() {
         this.list = new MyArrayList<E>();
     }
@@ -77,18 +67,6 @@ public class MyStack<E> implements StackADT<E> {
         return list.contains(toFind);
     }
 
-    /**
-     * Returns the index of the first occurrence of the specified element in this
-     * 
-     * @param toFind element to search for.
-     * @return the index of the first occurrence of the specified element in this
-     *        list, or -1 if this list does not contain the element.
-     * @throws NullPointerException if the specified element is null and this list
-     *       does not permit null elements.
-     * @precondition none
-     * @postcondition none
-     * @complexity O(n)
-     */
     @Override
     public int search(E toFind) {
         int index = list.indexOf(toFind);
@@ -98,47 +76,17 @@ public class MyStack<E> implements StackADT<E> {
         return size() - index;
     }
 
-    /**
-     * Returns an iterator over the elements in this list in proper sequence.
-     * 
-     * @param none
-     * @return an iterator over the elements in this list in proper sequence.
-     * @throws none
-     * @precondition none
-     * @postcondition none
-     * @complexity O(1)
-     */
     @Override
     public Iterator<E> iterator() {
         return list.iterator();
     }
 
-    /**
-     * Returns true if this list contains no elements.
-     * 
-     * @param none
-     * @return true if this list contains no elements, false otherwise.
-     * @throws none
-     * @precondition none
-     * @postcondition none
-     * @complexity O(1)
-     */
     @Override
     public boolean equals(StackADT<E> that) {
         // Implementation remains the same
         return super.equals(that);
     }
 
-    /**
-     * Returns the number of elements in this list.
-     * 
-     * @param none
-     * @return the number of elements in this list.
-     * @throws none
-     * @precondition none
-     * @postcondition none
-     * @complexity O(1)
-     */
     @Override
     public int size() {
         return list.size();
